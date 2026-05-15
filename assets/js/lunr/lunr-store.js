70415c501c90e65f2702fe6c8af37cc83a33706d
thomasburgess <thomasburgess@users.noreply.github.com>
2026-05-15 14:23:58 +0000

    jekyll build from Action b81c21c5c1e7b38f6c924fd974eba705453bafe3

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,156 @@
+/*
+Copyright (c) 2013-2020 Michael Rose and contributors
+MIT License
+From minimal-mistakes: https://github.com/mmistakes/minimal-mistakes/
+*/
+var store = [{
+    "title": "Archive",
+    "subtitle": "Thomas T Burgess’s Blog",
+    "excerpt": "",
+    "content": "",
+      "categories": null,
+      "date": null,
+      "tags": null,
+      "url": "https://thomasburgess.github.io/archive",
+      "img": "https://thomasburgess.github.io/assets/images/pages/planet7.png"
+    },{
+    "title": "Exploring repunit digit sum fractions",
+    "subtitle": "Down the integer sequences rabbit hole",
+    "excerpt": "The freaction of a repdigit and the sum of its digits … it is sometimes an integer.",
+    "content": "In which I go down a rabbit hole of integer sequences when trying to understand a pattern in fractions of repeated digits and their digit sums. The tweets Note: this...",
+      "categories": ["blog","math"],
+      "date": "2021-06-20",
+      "tags": ["maths","oeis"],
+      "url": "https://thomasburgess.github.io/2021/06/20/repdigits.html",
+      "img": "https://thomasburgess.github.io/assets/images/2021/06/20/APOD_Ganymede_repunit.jpg"
+    },{
+    "title": "Tree hack",
+    "subtitle": "Building and printing a tree",
+    "excerpt": "I had to write a python script that transforms incoming date organised into a  folder structure. For this, I needed both a simple python representation and an easy  way to print the data.",
+    "content": "Header image credit1 In which I made two snippets for my trees. Background Recently, I had to write a python script that transforms incoming data organised in a folder structure....",
+      "categories": ["blog","code"],
+      "date": "2021-07-18",
+      "tags": ["python","coding","trees"],
+      "url": "https://thomasburgess.github.io/2021/07/18/tree_hack.html",
+      "img": "https://thomasburgess.github.io/assets/images/2021/07/18/cristina-gottardi-wVTGdIGdojc-unsplash-crop.jpg"
+    },{
+    "title": "Polygon vertices",
+    "subtitle": "All the ways regular polygons tile the plane",
+    "excerpt": "There are only 21 ways to place regular polygons in gapless tilings. The point where  the polygons meet is called a vertex. Vertices are named by the polygons meeting  there. For instance, the vertex where four squares meet is 4.4.4.4. In this post,  I will make code to find and draw all possible vertices.",
+    "content": "Introduction There are only 21 ways to place regular polygons in gapless tilings. The point where the polygons meet is called a vertex. Vertices are named by the polygons meeting...",
+      "categories": ["blog","math"],
+      "date": "2022-04-17",
+      "tags": ["geometry","maths","polygon","python"],
+      "url": "https://thomasburgess.github.io/2022/04/17/Vertices.html",
+      "img": "https://thomasburgess.github.io/assets/images/2022/04/17/vertices.png"
+    },{
+    "title": "Parameter space",
+    "subtitle": "Parallel iteration over a parameter space",
+    "excerpt": "I often find myself having to run code for some arbitrary set of different  parameters. This can be done with nested for loops. This is not pretty, and poses  a challenge for paralleization and progress tracking.",
+    "content": "Header image credit1 Introduction I often find myself having to run code for some arbitrary set of different parameters. This can be done with nested for loops. This is not...",
+      "categories": ["blog"],
+      "date": "2022-05-31",
+      "tags": ["blog","coding","python"],
+      "url": "https://thomasburgess.github.io/2022/05/31/Parameter-Space.html",
+      "img": "https://thomasburgess.github.io/assets/images/2022/05/31/jupiter.png"
+    },{
+    "title": "Fun with fish tiles",
+    "subtitle": "",
+    "excerpt": "A fish tile is formed by two equilateral pentagons - the regular pentagon head, and the pentagonal crescent tail. It is remarkably fun to play with!",
+    "content": "This article was featured in Mitteiling der Meutschen Mathematiker-Vereingung1. The fish tile A fish tile is shown in Figure 1. It is remarkably fun to play with! It is formed...",
+      "categories": ["blog","math"],
+      "date": "2022-10-15",
+      "tags": ["tiling","geometry","math","n-gons","mathart"],
+      "url": "https://thomasburgess.github.io/2022/10/15/Fish-tiles.html",
+      "img": "https://thomasburgess.github.io/assets/images/2022/10/15/header_crop.png"
+    },{
+    "title": "Advent of Code 2022",
+    "subtitle": "A new problem, every day, all of december.",
+    "excerpt": "I will do this years Advent of Code 2022 in vanilla (only included libraries)  python 3.11. I’ve joined the challenge a few times before - but never finished.  Let’s see how many days I manage this round :D",
+    "content": "Advent of Code 2022 I will do this years Advent of Code 2022 in vanilla (only included libraries) python 3.10.4. I’ve joined the challenge a few times before - but...",
+      "categories": ["blog","advent","code"],
+      "date": "2022-12-01",
+      "tags": ["coding","advent","python"],
+      "url": "https://thomasburgess.github.io/2022/12/01/aoc22.html",
+      "img": "https://thomasburgess.github.io/assets/images/2022/12/01/aoc2022.png"
+    },{
+    "title": "Optimizing matrix multiplication",
+    "subtitle": "Speeding up sloww matrix operations in python",
+    "excerpt": "I was asked to speed the inner loop that runs many times inside a Markov Chain Monte  Carlo problem. This post is about different approaches to improving performance.  Possibly, some insights here are applicable beyond the details of the specific  problem.",
+    "content": "Header image credit1 Introduction I was asked to speed the inner loop that runs many times inside a Markov Chain Monte Carlo. The slowest part of each iteration is taking...",
+      "categories": ["blog"],
+      "date": "2023-02-02",
+      "tags": ["blog","python","numpy","coding","maths","matrix","mcmc"],
+      "url": "https://thomasburgess.github.io/2023/02/02/Matrix_Multiplication.html",
+      "img": "https://thomasburgess.github.io/assets/images/2023/02/02/mikita-yo-OCrl1Tkt630-unsplash.jpg"
+    },{
+    "title": "Advent of Code 2023",
+    "subtitle": "A new problem, every day, all of december.",
+    "excerpt": "I will do this years Advent of Code 2023 in vanilla (only included libraries)  python 3.11. I’ve joined the challenge a few times before - but never finished.  Let’s see how many days I manage this round :D",
+    "content": "Advent of Code 2023 Last year I posted about 16 problems from Advent of Code 2022. This year, I will do Advent of Code 2023 in vanilla (only included libraries)...",
+      "categories": ["blog","advent"],
+      "date": "2023-12-01",
+      "tags": ["coding","advent","python"],
+      "url": "https://thomasburgess.github.io/2023/12/01/advent2023.html",
+      "img": "https://thomasburgess.github.io/assets/images/2023/12/01/advent_shot.png"
+    },{
+    "title": "Rectangle areas!",
+    "subtitle": "Dad &amp; kid Exploring Areas of Rectangles and Squares.",
+    "excerpt": "Recently, I did a small exploration of the areas of rectangles and squares with my  7-year-old kid who loves learning about math. Here we consider rectangles with two  positive integer side lengths, a and b. The area of a rectangle is A = a*b, and its circumference is C=2a+2b. Together, we computed A for various a and b with increasing square side s.",
+    "content": "Dad &amp; kid Exploring Areas of Rectangles and Squares. Recently, I did a small exploration of the areas of rectangles and squares with my 7-year-old kid who loves learning about...",
+      "categories": ["blog","math"],
+      "date": "2024-07-30",
+      "tags": ["area","maths","kids"],
+      "url": "https://thomasburgess.github.io/2024/07/30/rectangles.html",
+      "img": "https://thomasburgess.github.io/assets/images/2024/07/30/skyline.png"
+    },{
+    "title": "Mersenne Primes.",
+    "subtitle": "Searching for rare numbers with python.",
+    "excerpt": "The 52nd Mersenne prime was discovered on October 21, 2024. Despite intense effort,  it took six years to surpass the 51st! It is the largest prime number ever found so  far!",
+    "content": "Introduction Update: I recorded myself reading 419 digits from the 52nd Mersenne prime for Say the prime. The 52nd Mersenne prime was discovered on October 21, 2024 (Source: GIMPS). Despite...",
+      "categories": ["blog","math"],
+      "date": "2024-10-26",
+      "tags": ["maths","primes","python","GIMPS"],
+      "url": "https://thomasburgess.github.io/2024/10/26/mersenne.html",
+      "img": "https://thomasburgess.github.io/assets/images/2024/10/26/mersennes.png"
+    },{
+    "title": "Blog setup",
+    "subtitle": "High expectations colliding with harsh reality…",
+    "excerpt": "This blog is built with Jekyll and is hosted for free on GitHun with GitHub pages. The  site is generated from markdown sources automatically on every push to the blog’s git  repository.",
+    "content": "Header photo credit1 Introduction This blog is built with Jekyll and and is hosted for free on GitHub with GitHub pages. The site is generated from markdown sources automatically on...",
+      "categories": ["blog"],
+      "date": "2024-10-30",
+      "tags": ["jekyll","github"],
+      "url": "https://thomasburgess.github.io/2024/10/30/Blog-setup.html",
+      "img": "https://thomasburgess.github.io/assets/images/2024/10/30/ville-palmu-ZTMqY6DEGRQ-unsplash.jpg"
+    },{
+    "title": "Say the prime",
+    "subtitle": "The 52nd Mersenne Prime digits 3772-4190.",
+    "excerpt": "The goal is to Say The Prime is to have people speak all the 41 million digits of the 52nd Mersenne prime number on a YouTube Playlist.",
+    "content": "Introduction Recently, I blogged about Mersenne prime numbers. Today, I saw the announcement of Say The Prime. Of course I will join the effort! The goal of Say The Prime...",
+      "categories": ["blog","math"],
+      "date": "2024-11-02",
+      "tags": ["maths","primes","youtube"],
+      "url": "https://thomasburgess.github.io/2024/11/02/saytheprime.html",
+      "img": "https://thomasburgess.github.io/assets/images/2024/11/02/saytheprime.png"
+    },{
+    "title": "Passwords with Polars",
+    "subtitle": "Wrangling csv password manager exports",
+    "excerpt": "I’ve used several approaches to password management. Not all passwords  have been migrated when switching solution. In this post, I’ll consolidate .csv  exports from different password managers, and analyze the resulting dataset. I’ll tackle this challenge with Polars.",
+    "content": "Header image credit1 Introduction Over time, I have tried several approaches to password management: LastPass, Firefox, and Safari (now Apple’s Passwords app). Not all passwords have been migrated when switching...",
+      "categories": ["blog","code"],
+      "date": "2024-11-04",
+      "tags": ["python","coding","polars","csv"],
+      "url": "https://thomasburgess.github.io/2024/11/04/Passwords-with-Polars.html",
+      "img": "https://thomasburgess.github.io/assets/images/2024/11/04/hans-jurgen-mager-ffE6g1p5mjc-unsplash.jpg"
+    },{
+    "title": "Tomato",
+    "subtitle": "Fixing my home wireless network",
+    "excerpt": "I installed FreshTomato to a R7000 WiFi router and made its 5Ghz interface a bridge to my 2.4Ghz ISP WiFi router.",
+    "content": "Header image credit1 Tomato A while back I got a GPD mini laptop running bazzite, it has a 7” screen and surprising amounts of power. However, at times I want...",
+      "categories": ["blog"],
+      "date": "2026-05-15",
+      "tags": ["blog","wifi","freshtomato","bazzite","linux","vpn","sunshine","moonlight"],
+      "url": "https://thomasburgess.github.io/2026/05/15/Tomato.html",
+      "img": "https://thomasburgess.github.io/assets/images/2026/05/15/tomato.jpg"
+    }]
